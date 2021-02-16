0ff076c45a0b8d1f07c844f5c3452b47997f2dc5
Amrit Shrestha <amritms@gmail.com>
2021-02-16 12:38:01 -0500

    add tests for book creation api

--- tests/Feature/Http/Controllers/BooksControllerTest.php
+++ tests/Feature/Http/Controllers/BooksControllerTest.php
@@ -48,4 +48,61 @@ class BooksControllerTest extends TestCase
         $this->assertEquals($data['published_at'], $book->published_at);
         $this->assertEquals($data['status'], $book->status);
     }
+
+    /** @test */
+    public function unauthorized_user_cannot_create_book()
+    {
+        $data = [
+            'title' => 'To Kill a Mockingbird',
+            'isbn' => '0978039912',
+            'published_at' => $this->faker->date('Y-m-d', date('Y-m-d', strtotime("-1 year"))),
+            'status' => 'AVAILABLE'
+        ];
+
+        $response = $this->postJson('/api/books', $data);
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @test
+     * @dataProvider validationDataProvider
+     */
+    public function user_cannot_create_book_with_invalid_data(array $invalidData, string $invalidParameter)
+    {
+        $user = User::factory()->create();
+
+        $validData = [
+            'title' => 'To Kill a Mockingbird',
+            'isbn' => '0978039912',
+            'published_at' => $this->faker->date('Y-m-d', date('Y-m-d', strtotime("-1 year"))),
+            'status' => 'AVAILABLE'
+        ];
+
+        $data = array_merge($validData, $invalidData);
+
+        $response = $this->actingAs($user)->postJson('/api/books', $data);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonValidationErrors([$invalidParameter]);
+    }
+
+    public function validationDataProvider()
+    {
+        return [
+            [['title' => null], 'title'],
+            [['title' => ''], 'title'],
+            [['title' => []], 'title'],
+            [['isbn' => 'null'], 'isbn'],
+            [['isbn' => ''], 'isbn'],
+            [['isbn' => []], 'isbn'],
+            [['isbn' => '0987654321'], 'isbn'],
+            [['published_at' => null], 'published_at'],
+            [['published_at' => ''], 'published_at'],
+            [['published_at' => []], 'published_at'],
+            [['published_at' => '2020-13-13'], 'published_at'],
+            [['published_at' => '11-25-2020'], 'published_at'],
+            [['published_at' => '12-12-2020'], 'published_at'],
+        ];
+    }
 }
